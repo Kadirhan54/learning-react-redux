8e4547f1ab120347706573b5420b6e21cea080a2
kadirhan <61658086+Kadirhan54@users.noreply.github.com>
2023-11-11 21:59:22 +0300

    project initialised

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-start justify-between p-24">
+      
+      <h1>Redux Toolkit Counter App</h1>
+
+    </main>
+  );
+}
